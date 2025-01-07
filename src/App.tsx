c0b91ed7c2c8ca04ca4c765084fdd8d99a5a8d4f
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2025-01-07 20:56:51 +0900

    feat: init 및 라우팅설정

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ChargePage from "./pages/account/charge";
+import HomePage from "./pages";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/account/charge" element={<ChargePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
