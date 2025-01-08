9c5ba0e9863b7c68cc1473d93f00e9f038f7f61a
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2025-01-08 20:28:03 +0900

    feat: 충전완료 UI, 라우팅 -#5

--- src/pages/account/charge/success.tsx
+++ src/pages/account/charge/success.tsx
@@ -1,7 +1,35 @@
+import { IoIosCheckmarkCircle } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
 const ChargeSuccessPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="relative m-5 border-[1px] border-solid w-96">
-      ChargeSuccessPage
+      <p className="bg-gray-100 text-center font-bold text-lg py-2">충전하기</p>
+      {/* 이미지는 아이콘으로 대체하겠습니다! */}
+      <div className="flex justify-center my-8">
+        <IoIosCheckmarkCircle className="w-20 h-20 text-blue-400" />
+      </div>
+      <p className="font-bold text-[18px] text-center mb-4">충전 결과</p>
+      <div className="flex mx-5 border-[1px] mb-48">
+        <div className="w-1/2 border-r-[1px] bg-gray-50">
+          <p className="p-3">충전 캐시</p>
+          <p className="p-3">보유 캐시 잔액</p>
+        </div>
+        <div className="w-1/2">
+          <p className="p-3 font-bold">
+            <b className="text-blue-400">1,000</b> 캐시
+          </p>
+          <p className="p-3 font-bold">750,000 캐시</p>
+        </div>
+      </div>
+      <button
+        onClick={() => navigate("/account/charge")}
+        className="bg-blue-400 w-[calc(100%-40px)] py-3 mx-5 mb-8 text-white font-bold text-[18px] rounded-lg disabled:bg-blue-300 disabled:cursor-not-allowed"
+      >
+        확인
+      </button>
     </div>
   );
 };
